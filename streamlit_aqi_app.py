4cd84a9cf210b3e00e8844c483fdd4f0af35bf1c
Jes <143019142+JesHP73@users.noreply.github.com>
2023-11-24 19:17:48 +0100

    Update streamlit_aqi_app.py

--- streamlit_aqi_app.py
+++ streamlit_aqi_app.py
@@ -48,6 +48,11 @@ filtered_data = filter_data(selected_countries)
 display_chart(filtered_data)
 
 
+import streamlit as st
+import pandas as pd
+import plotly.graph_objs as go
+from plotly.subplots import make_subplots
+
 # Define the WHO guideline values
 who_guidelines = {
     'NO2': 10,  # µg/m3 annual average
@@ -61,39 +66,37 @@ who_guidelines = {
 df = pd.read_csv('https://raw.githubusercontent.com/JesHP73/plotAQEU/main/eu_dataset_cleaned/aggregated_data_eu_air_quality.csv')
 df['WHO Guideline'] = df['air_pollutant'].map(lambda x: who_guidelines.get(x, 0))
 
-# If the data is loaded correctly, then proceed with plotting
-if 'plot_data' in locals():
-    # Streamlit app layout
-    st.title('Top Countries Exceeding WHO Air Pollutant Levels (AQI)')
+# Streamlit app layout
+st.title('Top Countries Exceeding WHO Air Pollutant Levels (AQI)')
 
-    # Create subplots
-    fig = make_subplots(specs=[[{"secondary_y": True}]])
+# Create subplots
+fig = make_subplots(specs=[[{"secondary_y": True}]])
 
-    # Add bar plot for each air pollutant
-    for pollutant in plot_data['air_pollutant'].unique():
-        subset = plot_data[plot_data['air_pollutant'] == pollutant]
-        fig.add_trace(go.Bar(x=subset['country'], 
-                             y=subset['AQI_Index'], 
-                             name=pollutant),
-                      secondary_y=False)
-
-    # Add scatter plot for WHO guidelines
-    fig.add_trace(go.Scatter(x=plot_data['country'], 
-                             y=plot_data['WHO Guideline'], 
-                             mode='lines+markers', 
-                             name='WHO Guideline', 
-                             marker=dict(color='red', size=10)),
+# Add bar plot for each air pollutant
+for pollutant in df['air_pollutant'].unique():
+    subset = df[df['air_pollutant'] == pollutant]
+    fig.add_trace(go.Bar(x=subset['country'], 
+                         y=subset['AQI_Index'], 
+                         name=pollutant),
                   secondary_y=False)
 
-    # Update layout
-    fig.update_layout(title_text='Top Countries Exceeding WHO Air Pollutant Levels (AQI)',
-                      xaxis_title='Country',
-                      yaxis_title='Air Quality Index (AQI)',
-                      legend=dict(orientation="h", yanchor="bottom", y=1.02, xanchor="right", x=1))
+# Add scatter plot for WHO guidelines
+fig.add_trace(go.Scatter(x=df['country'], 
+                         y=df['WHO Guideline'], 
+                         mode='lines+markers', 
+                         name='WHO Guideline', 
+                         marker=dict(color='red', size=10)),
+              secondary_y=False)
 
-    # Rotate x-axis labels
-    fig.update_xaxes(tickangle=45)
+# Update layout
+fig.update_layout(title_text='Top Countries Exceeding WHO Air Pollutant Levels (AQI)',
+                  xaxis_title='Country',
+                  yaxis_title='Air Quality Index (AQI)',
+                  legend=dict(orientation="h", yanchor="bottom", y=1.02, xanchor="right", x=1))
 
-    # Display the plot in the Streamlit app
-    st.plotly_chart(fig, use_container_width=True)
+# Rotate x-axis labels
+fig.update_xaxes(tickangle=45)
+
+# Display the plot in the Streamlit app
+st.plotly_chart(fig, use_container_width=True)
 
