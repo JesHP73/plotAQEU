e9cdfe4fc0ed08cd3783db968db0de7c2019a135
Jes <143019142+JesHP73@users.noreply.github.com>
2023-11-24 19:10:27 +0100

    Update streamlit_aqi_app.py

--- streamlit_aqi_app.py
+++ streamlit_aqi_app.py
@@ -49,6 +49,7 @@ filtered_data = filter_data(selected_countries)
 display_chart(filtered_data)
 
 
+
 # Define the WHO guideline values
 who_guidelines = {
     'NO2': 10,  # µg/m3 annual average
@@ -58,59 +59,57 @@ who_guidelines = {
     'CO': 4  # mg/m3 maximum daily 8-hour mean (or your approximation for annual average)
 }
 
-
 @st.cache
 def load_data():
-    url = 'https://github.com/JesHP73/plotAQEU/blob/203fa4c279cc3b606ce49b1432593348e1fbd9ad/eu_dataset_cleaned/aggregated_data_eu_air_quality.csv'  # Replace with your actual GitHub URL
-    headers = {'Accept': 'application/vnd.github.v3.raw'}  # Ensures you get the raw content from GitHub
-    response = requests.get(url, headers=headers)
+    # Make sure this URL is the raw version of the file on GitHub
+    url = 'https://raw.githubusercontent.com/JesHP73/plotAQEU/main/eu_dataset_cleaned/aggregated_data_eu_air_quality.csv'
+    response = requests.get(url)
     if response.status_code == 200:
         csv_raw = StringIO(response.content.decode('utf-8'))
         df = pd.read_csv(csv_raw)
         df['WHO Guideline'] = df['air_pollutant'].map(lambda x: who_guidelines.get(x, 0))
         return df
     else:
-        response.raise_for_status()  # Will raise an HTTPError if the HTTP request returned an unsuccessful status code
+        response.raise_for_status()
 
 try:
     plot_data = load_data()
-    st.write(plot_data)  # This will display the DataFrame in Streamlit if the data is loaded correctly.
 except requests.exceptions.HTTPError as e:
     st.error(f'Error fetching the CSV file: {e}')
 
-plot_data = load_data()
+# If the data is loaded correctly, then proceed with plotting
+if 'plot_data' in locals():
+    # Streamlit app layout
+    st.title('Top Countries Exceeding WHO Air Pollutant Levels (AQI)')
 
-# Streamlit app layout
-st.title('Top Countries Exceeding WHO Air Pollutant Levels (AQI)')
+    # Create subplots
+    fig = make_subplots(specs=[[{"secondary_y": True}]])
 
-# Create subplots
-fig = make_subplots(specs=[[{"secondary_y": True}]])
+    # Add bar plot for each air pollutant
+    for pollutant in plot_data['air_pollutant'].unique():
+        subset = plot_data[plot_data['air_pollutant'] == pollutant]
+        fig.add_trace(go.Bar(x=subset['country'], 
+                             y=subset['AQI_Index'], 
+                             name=pollutant),
+                      secondary_y=False)
 
-# Add bar plot for each air pollutant
-for pollutant in plot_data['air_pollutant'].unique():
-    subset = plot_data[plot_data['air_pollutant'] == pollutant]
-    fig.add_trace(go.Bar(x=subset['country'], 
-                         y=subset['AQI_Index'], 
-                         name=pollutant),
+    # Add scatter plot for WHO guidelines
+    fig.add_trace(go.Scatter(x=plot_data['country'], 
+                             y=plot_data['WHO Guideline'], 
+                             mode='lines+markers', 
+                             name='WHO Guideline', 
+                             marker=dict(color='red', size=10)),
                   secondary_y=False)
 
-# Add scatter plot for WHO guidelines
-fig.add_trace(go.Scatter(x=plot_data['country'], 
-                         y=plot_data['WHO Guideline'], 
-                         mode='lines+markers', 
-                         name='WHO Guideline', 
-                         marker=dict(color='red', size=10)),
-              secondary_y=False)
+    # Update layout
+    fig.update_layout(title_text='Top Countries Exceeding WHO Air Pollutant Levels (AQI)',
+                      xaxis_title='Country',
+                      yaxis_title='Air Quality Index (AQI)',
+                      legend=dict(orientation="h", yanchor="bottom", y=1.02, xanchor="right", x=1))
 
-# Update layout
-fig.update_layout(title_text='Top Countries Exceeding WHO Air Pollutant Levels (AQI)',
-                  xaxis_title='Country',
-                  yaxis_title='Air Quality Index (AQI)',
-                  legend=dict(orientation="h", yanchor="bottom", y=1.02, xanchor="right", x=1))
+    # Rotate x-axis labels
+    fig.update_xaxes(tickangle=45)
 
-# Rotate x-axis labels
-fig.update_xaxes(tickangle=45)
-
-# Display the plot in the Streamlit app
-st.plotly_chart(fig, use_container_width=True)
+    # Display the plot in the Streamlit app
+    st.plotly_chart(fig, use_container_width=True)
 
